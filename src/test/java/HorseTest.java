67275c7e5ec5f96171c3b5572c34244b792d9595
Alex_Holovko <tavrozaz@gmail.com>
2023-07-20 17:13:01 +0300

    Add "nullName" and "blankName" tests in HorseTes.

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,32 +1,25 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class HorseTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
     @Test
-    void getName() {
+    void nullName () {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 10, 100));
+
+        assertEquals("Name cannot be null.", e.getMessage());
     }
 
-    @Test
-    void getSpeed() {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  ", "\t\t", "\n"})
+    void blankName (String name) {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new Horse(name, 10, 100));
+
+        assertEquals("Name cannot be blank.", e.getMessage());
     }
 
-    @Test
-    void getDistance() {
-    }
-
-    @Test
-    void move() {
-    }
 }
